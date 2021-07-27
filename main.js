1d075dfa6a87a1f7e2b8d72ffdec86ad011fe94d
Giancarlo1021 <54759063+Giancarlo1021@users.noreply.github.com>
2021-07-27 11:36:05 -0700

    Add files via upload
    updated files

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
+function playRound(playerSelection, computerSelection) {
+   return console.log(playerSelection, computerSelection)
+}
 
+const playerSelection = window.prompt('Choose rock, paper, or scissors').toLowerCase();
+let result = false;
+ while (result === false){
+   if (playerSelection == 'rock' || 'paper' || 'scissors'){
+      result = true;
+   } else {
+      const playerSelection = window.prompt('Please Choose rock, paper, or scissors')
+      playerSelection.toLowerCase
+   }
+ }
 
+ const computerSelection = computerPlay();
+ console.log(playRound(playerSelection, computerSelection));
 
-
-
-
-function computerPlay (){
+ function computerPlay(){
    let value = Math.floor(Math.random() * 3);
    if (value === 0){
-    return "Rock";
+    return "rock";
    } else if (value === 1){
-    return "Paper";
+    return "paper";
    } else {
-    return "Scissor";
+    return "scissor";
    }
 }
